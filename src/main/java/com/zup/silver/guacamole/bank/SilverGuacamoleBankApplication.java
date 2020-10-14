a8a1e0e1baf9218456c1545e8a04e883c99de4a5
Alexandre de Lima <allima1991@gmail.com>
2020-10-14 00:48:23 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/zup/silver/guacamole/bank/SilverGuacamoleBankApplication.java
@@ -0,0 +1,13 @@
+package com.zup.silver.guacamole.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilverGuacamoleBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilverGuacamoleBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zup/silver/guacamole/bank/SilverGuacamoleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.silver.guacamole.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilverGuacamoleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
